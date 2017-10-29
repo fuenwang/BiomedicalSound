b7bb5301f395318a5353a386ac6b38c9c1d8675d
fuenwang <fulton84717@gmail.com>
2017-10-30 02:28:31 +0800

    gg

--- hw1/EE6265_HW1_106061531.m
+++ hw1/EE6265_HW1_106061531.m
@@ -5,10 +5,14 @@ pre_full = data.FUS_pre;
 post_full = data.FUS_post;
 fs = data.fs;
 window_size = 10;
+overlap_ratio = 0.75;
 
 
-window = Windows(pre_full, post_full, window_size);
+window = Windows(pre_full, post_full, window_size, overlap_ratio);
 final = false;
+t = 0;
+
 while ~final
+%while t < 30;
     [pre, post, final] = window.Next();
 end
--- hw1/Windows.m
+++ hw1/Windows.m
@@ -2,19 +2,27 @@ classdef Windows < handle
     properties
         idx
         w_size
+        ratio
         pre_sig
         post_sig
+        final
     end
     methods
-        function obj = Windows(pre, post, ws)
+        function obj = Windows(pre, post, ws, ratio)
             obj.idx = 1;
             obj.pre_sig = pre;
             obj.post_sig = post;
             obj.w_size = ws;
+            obj.ratio = ratio;
+            obj.final = false;
+            if ratio >= 1
+                error('overlap ratio > 1');
+            end
         end
         function [pre_frame, post_frame, final] = Next(obj) % Get next frame
             len = length(obj.pre_sig);
-            if obj.idx > len
+            jump = round((1 - obj.ratio) * obj.w_size);
+            if obj.final
                 error('Frame out of bound');
             end
             if obj.idx + obj.w_size - 1 > len
@@ -26,7 +34,8 @@ classdef Windows < handle
                 post_frame = obj.post_sig(obj.idx:(obj.idx + obj.w_size - 1));
                 final = false;
             end
-            obj.idx = obj.idx + obj.w_size;
+            obj.final = final;
+            obj.idx = obj.idx + jump;
         end
     end
 end
