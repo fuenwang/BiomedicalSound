913f32d2fcf517d2fee1f79dc7ec8ce903902fc0
Fu-En.Wang <fulton84717@gmail.com>
2017-10-30 19:41:01 +0800

    gg

--- hw1/EE6265_HW1_106061531.m
+++ hw1/EE6265_HW1_106061531.m
@@ -5,16 +5,27 @@ pre_full = data.FUS_pre;
 post_full = data.FUS_post;
 fs = data.fs;
 fc = data.fc;
-window_wavelength = 10;
-overlap_ratio = 0.75;
+window_wavelength = 6;
+overlap_ratio = 0;
 
 window_size = round(window_wavelength * (1 / fc) * fs);
 
 window = Windows(pre_full, post_full, window_size, overlap_ratio);
 final = false;
 t = 0;
-
+delay = cell(1, round(length(pre_full) / window_size));
 while ~final
 %while t < 30;
     [pre, post, final] = window.Next();
+    [val, lag] = xcorr(pre, post);
+    [~, idx] = max(val);
+    delay{t+1} = lag(idx);
+    %{
+    if t == 30
+        plot(lag, val)
+        break
+    end
+    %}
+    t = t + 1;
 end
+delay
