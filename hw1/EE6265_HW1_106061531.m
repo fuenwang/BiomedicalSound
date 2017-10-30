94e07ead76e6c2a1edc4498f0d909bc6d530ee8b
Fu-En.Wang <fulton84717@gmail.com>
2017-10-30 15:11:55 +0800

    gg

--- hw1/EE6265_HW1_106061531.m
+++ hw1/EE6265_HW1_106061531.m
@@ -4,9 +4,11 @@ data = load('FUS_RFData.mat');
 pre_full = data.FUS_pre;
 post_full = data.FUS_post;
 fs = data.fs;
-window_size = 10;
+fc = data.fc;
+window_wavelength = 10;
 overlap_ratio = 0.75;
 
+window_size = round(window_wavelength * (1 / fc) * fs);
 
 window = Windows(pre_full, post_full, window_size, overlap_ratio);
 final = false;
