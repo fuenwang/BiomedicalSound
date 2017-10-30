056a37a35de6582e1dde024e72ff70592c7845c9
Fu-En.Wang <fulton84717@gmail.com>
2017-10-30 22:06:42 +0800

    gg

--- hw1/EE6265_HW1_106061531.m
+++ hw1/EE6265_HW1_106061531.m
@@ -14,8 +14,11 @@ post_full = data.FUS_post;
 fs = data.fs * 1e6;
 fc = data.fc * 1e6;
 c0 = data.c0 * 1e-3 / 1e-6;
-window_wavelength = 10;
-overlap_ratio = 0;
+window_wavelength = 2;
+overlap_ratio = 0.5;
+resample_factor = 2;
+
+inter_step = 1 / resample_factor;
 
 window_size = round(window_wavelength * (1 / fc) * fs);
 
@@ -40,8 +43,13 @@ while ~final
     t = t + 1;
 end
 
-depth = c0 * center_idx * (1 / fs);
-delay_sec = delay * (1 / fs);
+depth_ori = c0 * center_idx * (1 / fs);
+depth = interp1(1:length(depth_ori), depth_ori, 1:inter_step:length(depth_ori), 'spline');
+
+delay_sec_ori = delay * (1 / fs);
+delay_sec = interp1(depth_ori, delay_sec_ori, depth, 'spline');
+
+
 tmp1 = [0 depth(1:end-1)];
 tmp2 = [0 delay_sec(1:end-1)];
 
@@ -49,7 +57,7 @@ delta_depth = depth - tmp1;
 delta_delay = delay_sec - tmp2;
 
 figure()
-plot(depth * 1e3, delay_sec * 1e-6);
+plot(depth * 1e3, delay_sec * 1e-6, 'Marker', 'o');
 title('Strain')
 xlabel('Depth(mm)')
 ylabel('Delay(us)')
